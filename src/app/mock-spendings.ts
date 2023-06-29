a64baf9fb233da17def82493e763cde7df450961
Ivan Mukhorin <mukhorinvanga@gmail.com>
2023-06-29 10:02:29 +0500

    add spendings placeholder

---
+++ src/app/mock-spendings.ts
@@ -0,0 +1,74 @@
+import { Spending } from './spending';
+
+export const SPENDINGS: Spending[] = [
+  {
+    amount: 925.0,
+    category: 'Garden',
+    date: new Date('2013-10-19T16:44:12.324Z'),
+    id: 1,
+  },
+  {
+    amount: 54.0,
+    category: 'Books',
+    date: new Date('2088-06-18T09:42:52.092Z'),
+    comment:
+      'The slim & simple Maple Gaming Keyboard from Dev Byte comes with a sleek body and 7- Color RGB LED Back-lighting for smart functionality',
+    id: 2,
+  },
+  {
+    amount: 485.0,
+    category: 'Books',
+    date: new Date('2037-03-18T16:43:01.623Z'),
+    comment:
+      'The beautiful range of Apple NaturalГ© that has an exciting mix of natural ingredients. With the Goodness of 100% Natural Ingredients',
+    id: 3,
+  },
+  {
+    amount: 948.0,
+    category: 'Music',
+    date: new Date('2089-09-01T04:10:49.607Z'),
+    id: 4,
+  },
+  {
+    amount: 733.0,
+    category: 'Movies',
+    date: new Date('2088-06-16T13:41:09.797Z'),
+    id: 5,
+  },
+  {
+    amount: 956.0,
+    category: 'Home',
+    date: new Date('2044-02-11T21:43:05.504Z'),
+    comment:
+      'The Nagasaki Lander is the trademarked name of several series of Nagasaki sport bikes, that started with the 1984 ABC800J',
+    id: 6,
+  },
+  {
+    amount: 19.0,
+    category: 'Sports',
+    date: new Date('2069-04-03T14:45:44.311Z'),
+    id: 7,
+  },
+  {
+    amount: 938.0,
+    category: 'Computers',
+    date: new Date('2034-08-20T16:27:08.961Z'),
+    comment:
+      'Ergonomic executive chair upholstered in bonded black leather and PVC padded seat and back for all-day comfort and support',
+    id: 8,
+  },
+  {
+    amount: 710.0,
+    category: 'Toys',
+    date: new Date('2033-04-01T17:35:28.501Z'),
+    id: 9,
+  },
+  {
+    amount: 984.0,
+    category: 'Games',
+    date: new Date('2013-11-12T15:44:39.592Z'),
+    comment:
+      'Carbonite web goalkeeper gloves are ergonomically designed to give easy fit',
+    id: 10,
+  },
+];
---
+++ src/app/spending.ts
@@ -0,0 +1,7 @@
+export interface Spending {
+  id: number;
+  amount: number;
+  category: string;
+  comment?: string;
+  date: Date;
+}
