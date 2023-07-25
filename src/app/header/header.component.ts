e224551f446ae8378be055d6565a5cb3ad44716d
LitleStuart <vanyamukhori@icloud.com>
2023-07-25 13:47:39 +0500

    pass data to header from server

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,8 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  user = {
-    name: 'Ivan',
-    balance: 10490,
-  };
+  @Input() name?: string;
+  @Input() balance?: number;
 }
